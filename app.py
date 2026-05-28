71460f6f3408fd462759ec34d7f8ec77dc5c25c9
lingudusailokesh <loki50672@gmail.com>
2026-05-28 17:28:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 import streamlit as st
 import joblib
-import numpy as np
+import pandas as pd
 
 # Load trained model
 model = joblib.load('model.pkl')
 
+# Load feature names
+feature_names = joblib.load('feature_names.pkl')
+
 # Title
 st.title("Ford Car Price Prediction")
 
-# User inputs
-year = st.number_input("Car Year")
-mileage = st.number_input("Mileage")
-engine_size = st.number_input("Engine Size")
+# User Inputs
+year = st.number_input("Car Year", min_value=2000, max_value=2030, step=1)
+mileage = st.number_input("Mileage", min_value=0)
+engine_size = st.number_input("Engine Size", min_value=0.0, step=0.1)
 
-# Prediction button
+# Predict Button
 if st.button("Predict Price"):
 
-    features = np.array([[year, mileage, engine_size]])
+    # Create empty dataframe with same columns used during training
+    input_data = pd.DataFrame(columns=feature_names)
 
-    prediction = model.predict(features)
+    # Fill all columns with 0
+    input_data.loc[0] = 0
 
+    # Add user inputs
+    input_data['year'] = year
+    input_data['mileage'] = mileage
+    input_data['engineSize'] = engine_size
+
+    # Prediction
+    prediction = model.predict(input_data)
+
+    # Show result
     st.success(f"Predicted Price: £{prediction[0]:,.2f}")
